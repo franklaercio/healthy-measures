0a056e2ae18f075f006e4d3e4f3f755f95052473
Frank <franklaercio@gmail.com>
2023-06-06 01:17:13 -0300

    test: adding body validator test

---
+++ src/test/java/br/ufrn/healthy/measures/unit/BodyValidatorTest.java
@@ -0,0 +1,10 @@
+package br.ufrn.healthy.measures.unit;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class BodyValidatorTest {
+
+
+}
